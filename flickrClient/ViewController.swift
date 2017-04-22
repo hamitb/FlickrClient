a0c11d26199d9e0352d5caff94cd87f1cfeb4feb
Hamit Burak Emre <hamit@otsimo.com>
2017-04-22 23:57:11 +0300

    Initial Commit

---
+++ flickrClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flickrClient
+//
+//  Created by Hamit Burak Emre on 22/04/17.
+//  Copyright © 2017 Hamit Burak Emre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
